7db241f433d899673b1f7a7d37c1059ee8f3c603
Eduardo Briceño <eduardojose@gmail.com>
2023-11-02 09:23:22 +0100

    Add navigation component placeholder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "73bed94920cf2ed9"; }
+/******/ 		__webpack_require__.h = function() { return "975fd3e085f46cd1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,7 @@
+export default function Navigation() {
+    return (
+        <nav>
+            <h1>GH Assessment</h1>
+        </nav>
+    )
+}
